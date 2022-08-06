f2bc8d66a553fecb00824f5d8fed42f9a706e131
Shridhar Kulkarni <kshridhar.999@gmail.com>
2022-08-06 09:55:50 +0530

    all done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Liked from "./liked";
@@ -8,7 +8,8 @@ const Home = () => {
   let isSigned = window.localStorage.getItem("isSigned")?JSON.parse(window.localStorage.getItem("isSigned")):false
   const [user, setUser] = useState([]);
   const [vidName, setVidName] = useState("");
-  const [vidList, setVidList] = useState([{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="},{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="},{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="},{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="},{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="},{url:"media.istockphoto.com/photos/hand-holding-heart-against-sparkling-golden-bokeh-lights-picture-id1193218253?k=20&m=1193218253&s=612x612&w=0&h=867Eq3yPsTe0ReLD2GEjicWH-6Q-TB_N1k9KHcmtfUU="} ]);
+  const [vidList, setVidList] = useState([{url:"/Users/cogoport/download_vids/a.mp4", title:"a"},{url:"/Users/cogoport/download_vids/b.mp4", title:"b"}
+  ]);
   let navigate = useNavigate();
 
   const Signin = ()=> {
@@ -20,16 +21,20 @@ const Home = () => {
   }
 
   const getVids = async () => {
-    let allVideos = await axios.get("http://127.0.0.1:5000/videos");
+    let allVideos = await axios.get("https://sshtube-app.herokuapp.com/videos");
+    allVideos=allVideos.data
     setVidList(allVideos);
   }
+  useEffect(()=>{
+    getVids();
+  }, []);
   return (
     <div>
       <div className="navBar">
           <div>
           {isSigned?
           (<><Link to="/user"><img src="https://us.123rf.com/450wm/koblizeek/koblizeek2001/koblizeek200100050/138262629-man-icon-profile-member-user-perconal-symbol-vector-on-white-isolated-background-.jpg?ver=6" width="50" height="50" alt="img" className="leftOptions"></img></Link>
-          <Link to="/uploads" className="leftOptions">Uploads</Link> </> 
+          <div class="circle" onClick={()=>{window.open("http://127.0.0.1:5000/upload", '_blank');}}></div></>
           ):(<><Link to="/signin"><img src="https://us.123rf.com/450wm/koblizeek/koblizeek2001/koblizeek200100050/138262629-man-icon-profile-member-user-perconal-symbol-vector-on-white-isolated-background-.jpg?ver=6" width="20" height="20"alt="img" className="leftOptions"></img></Link>
           <Link to="/signin" className="leftOptions">Uploads</Link></>)
           }
@@ -41,7 +46,7 @@ const Home = () => {
           </div>
 
           <div>
-            {isSigned? ///////////////////////////
+            {isSigned?
             (<><Link to={"/watch_history"} className="rightOptions">History</Link>
             <Link to={"/saved"} className="rightOptions">Saved</Link>
             <Link to={"/liked"} className="rightOptions">Liked</Link></>):
@@ -54,12 +59,14 @@ const Home = () => {
         {vidList.map((vid) => {
           return (
             <>
-            <Link to={"/video"} >
+            <Link to={"/video/"+vid.id} >
               <div className="vidCard">
-              <img src={"https://"+vid.url} height="200" width="300"></img>
+              <img src={vid.description} height="200" width="300" alt="some vid">
+              </img>
+              {console.log(vid.description)}
               <div className="bottomInfo">
-              <p className="title">My name is anthony gonz Please state the value of your mind.</p>
-              <div>Likes:likeCount</div>
+              <p className="title">{vid.title}</p>
+              <div>Likes:{vid.likeCount}</div>
               </div>
               </div>
             </Link>
